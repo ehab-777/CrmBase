8962f61b65eff89326898fe2303f68c785bacf60
Ehab Abuelsoud <ehab.abuelsoud@gmail.com>
2025-05-17 09:39:17 +0300

    Add Customer and SalesFollowup models

--- models.py
+++ models.py
@@ -30,4 +30,45 @@ class SalesPerson(db.Model):
     created_at = db.Column(db.DateTime, default=db.func.now())
     
     def __repr__(self):
-        return f'<SalesPerson {self.username}>' 
+        return f'<SalesPerson {self.username}>'
+
+class Customer(db.Model):
+    __tablename__ = 'customers'
+    
+    customer_id = db.Column(db.Integer, primary_key=True)
+    company_name = db.Column(db.String(100), nullable=False)
+    contact_person = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+    email_address = db.Column(db.String(100))
+    company_address = db.Column(db.String(200))
+    lead_source = db.Column(db.String(50))
+    initial_interest = db.Column(db.String(200))
+    company_industry = db.Column(db.String(100))
+    contact_person_position = db.Column(db.String(100))
+    assigned_salesperson_id = db.Column(db.Integer, db.ForeignKey('sales_team.salesperson_id'), nullable=False)
+    tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'), nullable=False)
+    date_added = db.Column(db.DateTime, default=db.func.now())
+    
+    def __repr__(self):
+        return f'<Customer {self.company_name}>'
+
+class SalesFollowup(db.Model):
+    __tablename__ = 'sales_followup'
+    
+    followup_id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.customer_id'), nullable=False)
+    assigned_salesperson_id = db.Column(db.Integer, db.ForeignKey('sales_team.salesperson_id'), nullable=False)
+    last_contact_date = db.Column(db.DateTime, nullable=False)
+    last_contact_method = db.Column(db.String(50))
+    summary_last_contact = db.Column(db.Text)
+    next_action = db.Column(db.String(50))
+    next_action_due_date = db.Column(db.DateTime)
+    current_sales_stage = db.Column(db.String(50))
+    potential_deal_value = db.Column(db.Float)
+    notes = db.Column(db.Text)
+    tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    created_by = db.Column(db.Integer, db.ForeignKey('sales_team.salesperson_id'), nullable=False)
+    
+    def __repr__(self):
+        return f'<SalesFollowup {self.followup_id}>' 
