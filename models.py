6528825509b8a55f39d619bf2ce119c01c127c75
Ehab Abuelsoud <ehab.abuelsoud@gmail.com>
2025-05-17 09:32:12 +0300

    Add SalesPerson model

--- models.py
+++ models.py
@@ -11,4 +11,22 @@ class Tenant(db.Model):
     created_at = db.Column(db.DateTime, default=db.func.now())
     
     def __repr__(self):
-        return f'<Tenant {self.name}>' 
+        return f'<Tenant {self.name}>'
+
+class SalesPerson(db.Model):
+    __tablename__ = 'sales_team'
+    
+    salesperson_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    salesperson_name = db.Column(db.String(50), nullable=False)
+    work_email = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(20))
+    role = db.Column(db.String(20), default='salesperson')
+    tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    
+    def __repr__(self):
+        return f'<SalesPerson {self.username}>' 
